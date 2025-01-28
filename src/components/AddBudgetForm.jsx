80aa94697eca88ec2c50bf9fbc7c799e05add379
Neeti05 <155546757+Neeti05@users.noreply.github.com>
2025-01-28 22:07:37 +0530

    Update AddBudgetForm.jsx

--- src/components/AddBudgetForm.jsx
+++ src/components/AddBudgetForm.jsx
@@ -46,7 +46,7 @@ const AddBudgetForm = () => {
             step="0.01"
             name="newBudgetAmount"
             id="newBudgetAmount"
-            placeholder="e.g., $350"
+            placeholder="e.g., ₹350"
             required
             inputMode="decimal"
           />
